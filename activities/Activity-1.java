7ead76e0c3746137f8a48b643f26eb4331c5c8c0
Henry_ <136041693+Henryqui@users.noreply.github.com>
2023-09-10 01:20:43 -0300

    Update and rename Activity-1 to Activity-1.java

---
+++ activities/Activity-1.java
@@ -0,0 +1,15 @@
+// 1)	Faça um programa que escreva de 30 a 50 contanto de 2 em 2
+
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        for (int x = 30; x<=50; x+=2){
+            System.out.println(x);
+        }
+
+    }
+}
+
